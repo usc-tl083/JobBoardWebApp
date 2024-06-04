8baae9b6b93778c8252b34cb31afce3014d76358
Tshering Pelmo Lama <t_l083@student.usc.edu.au>
2024-06-04 15:34:24 +1000

    Created login and register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import Spinner from "components/Spinner";
+import Layout from "layout/Layout";
+import {
+  Login,
+} from "pages";
+import { Suspense } from "react";
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello, It's my first React App!</h1>
-            <h2>Happy Coding!!</h2>
-        </div>
-    );
-};
+function App() {
+  return (
+    <Suspense
+      fallback={
+        <Layout>
+          <Spinner size={100} />
+        </Layout>
+      }
+    >
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route index element={<h1>Hello World</h1>} />
+      </Routes>
+    </Suspense>
+  );
+}
 
 export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,131 @@
+// import { useGoogleLogin } from "@react-oauth/google";
+import { Button, HelperText, Input, Label } from "@windmill/react-ui";
+// import ForgotPasswordModal from "components/ForgotPasswordModal";
+import { useUser } from "context/UserContext";
+import Layout from "layout/Layout";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { Link, Navigate, useLocation } from "react-router-dom";
+import PulseLoader from "react-spinners/PulseLoader";
+import authService from "services/auth.service";
+
+const Login = () => {
+    const { isLoggedIn, setUserState } = useUser();
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [redirectToReferrer, setRedirectToReferrer] = useState(false);
+    const { state } = useLocation();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = async (data) => {
+        const { email, password } = data;
+
+        try {
+            setError("");
+            setIsLoading(true);
+            const data = await authService.login(email, password);
+            toast.success("Login successful 🔓");
+
+            setTimeout(() => {
+                setUserState(data);
+                setRedirectToReferrer(true);
+                setIsLoading(false);
+            }, 1500);
+        } catch (error) {
+            setIsLoading(false);
+            setError(error.response?.data.message);
+        }
+    };
+
+    if (redirectToReferrer) {
+        return <Navigate to={state?.from || "/"} />;
+    }
+    if (isLoggedIn) {
+        return <Navigate to={state?.from || "/"} />;
+    }
+
+    return (
+        <Layout title="Login">
+            <div className="flex items-center justify-center m-auto mt-20">
+                <form
+                    className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-full md:w-1/2"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <h1 className="text-center text-4xl my-4">Login to continue</h1>
+                    <div className="">
+                        <Label className="block text-grey-darker text-sm font-bold mb-2">
+                            <span>Email</span>
+                        </Label>
+                        <Input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                            type="email"
+                            name="email"
+                            {...register("email", {
+                                required: true,
+                                // eslint-disable-next-line no-useless-escape
+                                pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                            })}
+                            placeholder="Enter a valid email"
+                        />
+                    </div>
+                    {errors?.email && errors?.email.type === "required" && (
+                        <HelperText className="mt-1 italic" valid={false}>
+                            Email required
+                        </HelperText>
+                    )}
+                    {errors?.email && errors?.email.type === "pattern" && (
+                        <HelperText className="mt-1 italic" valid={false}>
+                            Invalid email
+                        </HelperText>
+                    )}
+                    <div className="mt-4">
+                        <Label className="block text-grey-darker text-sm font-bold mb-2">
+                            <span>Password</span>
+                        </Label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                            type="password"
+                            name="password"
+                            {...register("password", { required: true })}
+                        />
+                    </div>
+                    {errors?.password && (
+                        <HelperText className="mt-1 italic" valid={false}>
+                            {errors?.password?.type === "required" && "Password required"}
+                        </HelperText>
+                    )}
+                    {error && (
+                        <HelperText className="mt-1 italic" valid={false}>
+                            {error}
+                        </HelperText>
+                    )}
+                    {/* <div className="mt-4">
+                        <ForgotPasswordModal />
+                    </div> */}
+                    <Button type="submit" disabled={isLoading}>
+                        {isLoading ? <PulseLoader color={"#0a138b"} size={10} loading /> : "Login"}
+                    </Button>
+                    <p className="text-sm mt-4">
+                        Don&apos;t have an account?{" "}
+                        <Link to="/" className="font-bold">
+                            Sign Up
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </Layout>
+    );
+};
+
+export default Login;
