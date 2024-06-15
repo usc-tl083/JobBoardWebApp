520d0d0f6d1123127c4dba18d2eb4669624e4398
Tshering Pelmo Lama <t_l083@student.usc.edu.au>
2024-06-15 11:41:10 +1000

    Minor fixes to initialization codes.

--- server/config/index.js
+++ server/config/index.js
@@ -3,20 +3,21 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 const database =
-    process.env.NODE_ENV === "test"
-        ? process.env.POSTGRES_DB_TEST
-        : process.env.POSTGRES_DB;
+  process.env.NODE_ENV === "test"
+    ? process.env.POSTGRES_DB_TEST
+    : process.env.POSTGRES_DB;
 
-const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${database}`;
+// const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${database}`;
+const connectionString = `postgressql://user:postgres@localhost:1234/database`;
 const pool = new Pool({
-    connectionString,
-    /*
+  connectionString,
+  /*
       SSL is not supported in development
       */
-    ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
-    end: () => pool.end(),
+  query: (text, params) => pool.query(text, params),
+  end: () => pool.end(),
 };
